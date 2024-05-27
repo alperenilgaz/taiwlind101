0f3239110da5abe8bd92077d42a3506de5e0cbf3
Alperen İlgaz <alperenilgaz3@gmail.com>
2024-05-27 03:19:18 +0300

    update:theme

--- src/Component/Context/ThemeProvider.jsx
+++ src/Component/Context/ThemeProvider.jsx
@@ -5,34 +5,27 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 const getStoredTheme = () => localStorage.getItem("theme") || "";
-const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
 
-const applyTheme = (theme) => {
-  if (theme === "dark") {
-    
-    document.body.classList.add("bg-slate-800", "text-white");
-    document.documentElement.classList.add("dark")
-    document.body.classList.remove("bg-white", "text-black");
-    
-  } else {
-    document.body.classList.add("bg-white", "text-black");
-    document.documentElement.classList.remove("dark")
-    document.body.classList.remove("bg-slate-800", "text-white");
-    
+const darkmode = (theme) => {
+  const previousTheme = localStorage.getItem("theme");
+  if (previousTheme) {
+    document.documentElement.classList.remove(previousTheme);
   }
+  if (theme) {
+    document.documentElement.classList.toggle(theme);
+    localStorage.setItem("theme", theme);
+  } 
 };
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(getStoredTheme);
 
   useEffect(() => {
-    setStoredTheme(theme);
-    applyTheme(theme);
+    darkmode(theme);
   }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
- 
       {children}
     </ThemeContext.Provider>
   );
